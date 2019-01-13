e2e490bf568dcf83f92d855ced072072a3b705ca
oli <olivrg@gmail.com>
2019-01-13 19:29:50 +0000

    separate product controller from the product manager

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import './products.dart';
 
+import './product_control.dart';
+
 class ProductManager extends StatefulWidget {
   final String startingProduct;
 
@@ -32,21 +34,19 @@ class _ProductManagerState extends State<ProductManager> {
     super.didUpdateWidget(oldWidget);
   }
 
+  void _addProduct(String product) {
+    setState(() {
+      _products.add(product);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     print('[ProductManager State] build()');
     return Column(children: [
       Container(
         margin: EdgeInsets.all(10.0),
-        child: RaisedButton(
-          color: Theme.of(context).primaryColor,
-          onPressed: () {
-            setState(() {
-              _products.add('Advanced Food Tester');
-            });
-          },
-          child: Text('Add Product'),
-        ),
+        child: ProductControl(_addProduct),
       ),
       Products(_products)
     ]);
